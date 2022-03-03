4c86a07fc3cf65796d81c0116b77d7229ed760d2
seoyoung <seoyoung1113@naver.com>
2022-03-04 04:18:02 +0900

    :sparkles: elasticSearch 데이터 삽입 시 id 설정 (#1)
    elasticSearch 데이터 삽입 시 _index,_type,_id,_source를 직접 정의하는 것으로 코드를 바꿈

--- elasticSearch/elasticSearch_crud.py
+++ elasticSearch/elasticSearch_crud.py
@@ -1,14 +1,16 @@
-from elasticsearch import helpers,Elasticsearch
+from elasticsearch import helpers, Elasticsearch
 import json
 
 sites = ['happybean', 'kakao']
-    
+
+
 def insertData():
     for site in sites:
-        with open(f'data\{site}.json', 'r', encoding = 'utf-8') as f:
+        with open(f'data\{site}.json', 'r', encoding='utf-8') as f:
             data = json.loads(f.read())
-        res = helpers.bulk(es, data, index="campaigns", doc_type=site)
-    
+        res = helpers.bulk(es, data)
+
+
 if __name__ == '__main__':
     es = Elasticsearch('http://localhost:9200/')
     insertData()
