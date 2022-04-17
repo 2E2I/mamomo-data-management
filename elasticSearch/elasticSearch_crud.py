b9d62139801210d10f80905ec90d4e4ce5b4c509
im-shung <seoyoung1113@gmail.com>
2022-04-17 19:09:37 +0900

    :label: 캠페인에 heartCount 필드 추가 및 초기화 (close #31)

--- elasticSearch/elasticSearch_crud.py
+++ elasticSearch/elasticSearch_crud.py
@@ -1,34 +1,36 @@
-from operator import index
 from elasticsearch import helpers, Elasticsearch
 import json
 
-sites = ['happybean', 'kakao','thedirectdonation', 'cherry']
+sites = ['happybean', 'kakao', 'thedirectdonation', 'cherry']
+
 
 def createIndex():
-    if not es.indices.exists(index = "campaigns"):
+    if not es.indices.exists(index="campaigns"):
         return es.indices.create(
-            index = "campaigns",
-            mappings= {
-                    "properties" : {
-                        "campaign_id" : {"type" : "keyword"},
-                        "site_type" : {"type" : "keyword"},
-                        "url" : {"type" : "keyword"},
-                        "title" : {"type" : "text"},
-                        "body" : {"type" : "text"},
-                        "organization_name" : {"type" : "keyword"},
-                        "thumbnail" : {"type" : "keyword"},
-                        "due_date" : {"type" : "date"},
-                        "start_date" : {"type" : "date"},
-                        "target_price" : {"type" : "long"},
-                        "status_price" : {"type" : "long"},
-                        "percent" : {"type" : "integer"}
+                index="campaigns",
+                mappings={
+                    "properties": {
+                        "campaign_id": {"type": "keyword"},
+                        "site_type": {"type": "keyword"},
+                        "url": {"type": "keyword"},
+                        "title": {"type": "text"},
+                        "body": {"type": "text"},
+                        "organization_name": {"type": "keyword"},
+                        "thumbnail": {"type": "keyword"},
+                        "due_date": {"type": "date"},
+                        "start_date": {"type": "date"},
+                        "target_price": {"type": "long"},
+                        "status_price": {"type": "long"},
+                        "percent": {"type": "integer"},
+                        "heartCount": {"type": "integer"}
                     }
                 }
-    )
+        )
+
 
 def initData():
     createIndex()
-    
+
     for site in sites:
         with open(f'data/{site}.json', 'r', encoding='utf-8') as f:
             data = json.loads(f.read())
@@ -37,5 +39,7 @@ def initData():
 
 
 if __name__ == '__main__':
-    es = Elasticsearch('http://elastic:hansung@localhost:9200/', timeout=30, max_retries=10, retry_on_timeout=True)
+    es = Elasticsearch('http://elastic:hansung@localhost:9200/', timeout=30,
+                       max_retries=10, retry_on_timeout=True)
+
     initData()
